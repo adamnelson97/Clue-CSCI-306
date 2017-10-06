9e223952985d508c7d05ed8f73381a7c9b61e297
Adam Nelson <adamnelson@mines.edu>
2017-10-05 23:13:07 -0600

    Added IntBoardTests to tests package

---
+++ src/tests/IntBoardTests.java
@@ -0,0 +1,23 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import experiment.IntBoard;
+
+public class IntBoardTests {
+
+	@Before
+	public void setUp() {
+		System.out.println("In @Before");
+		IntBoard board = new IntBoard();
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
