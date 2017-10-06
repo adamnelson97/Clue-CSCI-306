414007cc558ce2d92982e97cdc197547a54880b3
Adam Nelson <adamnelson@mines.edu>
2017-10-05 23:18:28 -0600

    Added test method stubs

--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -7,6 +7,14 @@ import org.junit.Test;
 
 import experiment.IntBoard;
 
+/**
+ * @author AdamN
+ *
+ */
+/**
+ * @author AdamN
+ *
+ */
 public class IntBoardTests {
 
 	@Before
@@ -14,10 +22,49 @@ public class IntBoardTests {
 		System.out.println("In @Before");
 		IntBoard board = new IntBoard();
 	}
-	
+
 	@Test
-	public void test() {
+	public void testTopLeftCorner() {
+		//tests adjacency list for [0][0]
 		fail("Not yet implemented");
 	}
+
+	@Test
+	public void testBottomRightCorner() {
+		//tests adjacency list for [3][3]
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRightEdge() {
+		//tests adjacency list for [1][3]
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLeftEdge() {
+		//tests adjacency list for [3][0]
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test2ndRow2ndCol() {
+		//tests adjacency list for [1][1], remember zero indexing
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test3rdRow3rdCol() {
+		//tests adjacency list for [2][2]
+		fail("Not yet implemented");
+	}
+
+	//Need six other test methods
+	//Template below
+
+	/*@Test
+	public void test() {
+		fail("Not yet implemented");
+	}*/
 
 }
