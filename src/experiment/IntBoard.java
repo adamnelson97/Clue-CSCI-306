8e42d1f13ac65ef981ad6c32cb3b9175e7db3a3d
Adam Nelson <adamnelson@mines.edu>
2017-10-05 23:01:14 -0600

    Added methods to IntBoard

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -13,5 +13,23 @@ public class IntBoard {
 	public IntBoard() {
 		// TODO IntBoard constructor
 	}
+	
+	//Methods
+	public void calcAdjacencies() {
+		//Stores adjacency matrix in adjMtx
+	}
+	
+	public void calcTargets(BoardCell startCell, int pathLength) {
+		//Stores possible target cells in targets
+	}
+	
+	public Set<BoardCell> getTargets() {
+		return targets; //Getter for targets so the set doesn't have to be created for every get
+	}
+	
+	public Set<BoardCell> getAdjList(BoardCell cell) {
+		Set<BoardCell> list = adjMtx.get(cell); //Stores the adjacency list for a cell in a new list
+		return list;
+	}
 
 }
