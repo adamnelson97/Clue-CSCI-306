712dce09ca464cbf83867916342e5e658face95e
Nathaniel Fuller <nfuller@mines.edu>
2017-10-09 08:41:32 -0600

    Added BadConfigFormatException skeletion.

---
+++ src/clueGame/BadConfigFormatException.java
@@ -0,0 +1,19 @@
+/*
+ * Class: BadConfigFormatException
+ * 
+ * Authors: Nathaniel Fuller, Adam Nelson
+ * 
+ * Purpose: An exception to be thrown when reading data from an improperly formatted config file.
+ * 
+ */
+
+package clueGame;
+
+class BadConfigFormatException extends Exception {
+	// -- Variables --
+	
+	// -- Constructors --
+	public BadConfigFormatException() {
+		// TODO: Implement constructors for BadConfigFormatException
+	}
+}
