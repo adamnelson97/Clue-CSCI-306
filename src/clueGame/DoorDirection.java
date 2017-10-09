f0f5dc143169b459109efdd3c3a04b6a42da94af
Nathaniel Fuller <nfuller@mines.edu>
2017-10-09 08:36:36 -0600

    Added DoorDirection enum skeleton

---
+++ src/clueGame/DoorDirection.java
@@ -0,0 +1,16 @@
+/*
+ * Class: DoorDirection
+ * 
+ * Authors: Nathaniel Fuller, Adam Nelson
+ * 
+ * Purpose: An enumerated type to indicate which direction a door is facing for the purposes of our 'Clue' game.
+ * 
+ */
+
+package clueGame;
+
+enum DoorDirection {
+	UP, DOWN, LEFT, RIGHT, NONE;
+	
+	// TODO: Implement additional functionality to DoorDirection as needed.
+}
