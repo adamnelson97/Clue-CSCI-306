b59e54581842bdd45a7d05414b180484422357c9
Adam Nelson <[adamnelson@mines.edu]>
2017-10-10 11:30:28 -0600

    Added block to create error log with parameterized exception constructor

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -9,15 +9,21 @@
 
 package clueGame;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class BadConfigFormatException extends Exception {
 	// -- Variables --
-	
+
 	// -- Constructors --
 	public BadConfigFormatException() {
 		super("Error: Bad Configuration Files");
 	}
-	
-	public BadConfigFormatException(String s) {
+
+	public BadConfigFormatException(String s) throws IOException {
 		super(s);
+		PrintWriter writer = new PrintWriter("BadConfigErrorLog.txt");
+		writer.println(s);
+		writer.close();
 	}
 }
