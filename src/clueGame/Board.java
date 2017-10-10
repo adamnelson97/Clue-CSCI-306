5e3c924bc764a2175fcf0ab0999c624f288c1d5b
Adam Nelson <[adamnelson@mines.edu]>
2017-10-10 12:21:01 -0600

    Added to loadBoardConfig to get the number of rows and columns. Ensures the board is a consistent size.

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -25,12 +25,12 @@ public class Board {
 	private Set<BoardCell> visited;
 	private String boardConfigFile;
 	private String roomConfigFile;
-	
+
 	// Variable used for the singleton pattern
 	private static Board theInstance = new Board();
 
 	// -- Constructors --
-	
+
 	// Private constructor to ensure only one can be created
 	private Board() {
 		// Set up empty data structures
@@ -39,7 +39,7 @@ public class Board {
 		targets = new HashSet<BoardCell>();
 		visited = new HashSet<BoardCell>();
 	}
-	
+
 	// -- Methods --
 	public void initialize() {
 		// TODO: figure out how to input the file names
@@ -47,40 +47,86 @@ public class Board {
 		try {
 			loadRoomConfig();
 		} catch (BadConfigFormatException e) {}
-		loadBoardConfig();
+		try {
+			loadBoardConfig();
+		} catch (BadConfigFormatException e) {}
 	}
-	
+
 	public void loadRoomConfig() throws BadConfigFormatException {
 		// TODO: Implement loadRoomConfig to read data from roomConfigFile.
 		FileReader roomCfg = null;
-		
+
 		try {
 			roomCfg = new FileReader(roomConfigFile);
-		} catch (FileNotFoundException e) {System.out.println("File not found."); }
-		
+		} catch (FileNotFoundException e) {System.out.println("Room Config File not found."); }
+
 		Scanner in = new Scanner(roomCfg);
 		char letter; //Ex: 'A'
 		String name; //Ex: 'Art Room'
 		String temp; //Entire line that will be manipulated, ex: A, Art Room, Card
 		int secComma; //Index location of second comma, i.e. follows the name of the room
-		
+
 		while (in.hasNextLine()) {
 			temp = in.nextLine(); //Takes in entire line, ex: A, Art Room, Card
 			if (!temp.endsWith("Card") && !temp.endsWith("Other")) {
 				throw new BadConfigFormatException("Room is not type Card or Other");
 			}
-			
+
 			letter = temp.charAt(0); //Stores the first character as the room symbol, ex: A
 			secComma = temp.indexOf(',', 1); //Char at index should be a comma, so the next one follows the name of the room
 			name = temp.substring(3, secComma);
 			legend.put(letter, name); //Adds room to the legend
 		}
 	}
-	
-	public void loadBoardConfig() {
+
+	public void loadBoardConfig() throws BadConfigFormatException {
 		// TODO: Implement loadBoardConfig to read data from boardConfigFile.
+		FileReader boardCfg = null;
+
+		try {
+			boardCfg = new FileReader(boardConfigFile);
+		} catch (FileNotFoundException e) {System.out.println("Board Config File not found."); }
+
+		//First, get dimensions of csv file so the board array can be initialized
+		Scanner in = new Scanner(boardCfg);
+		int expectedCommas = 0;
+
+		//Get the num of columns in the first row. All other rows need to have this many columns
+		String currLine = in.nextLine();
+		int numCommas = 0;
+		for (int i = 0; i < currLine.length(); i++) {
+			if (currLine.charAt(i) == ',') {
+				numCommas++; //Increments for each instance of a comma
+			}
+		}
+		expectedCommas = numCommas; //One less comma than total columns
+		numRows++; //Since a line has been read, this is the first row
+		
+		//Test all other rows to ensure they are the same size
+		while (in.hasNextLine()) {
+			currLine = in.nextLine();
+			numCommas = 0;
+			for (int i = 0; i < currLine.length(); i++) {
+				if (currLine.charAt(i) == ',') {
+					numCommas++; //Increments for each instance of a comma
+				}
+			}
+			if (numCommas != expectedCommas) {
+				throw new BadConfigFormatException("Number of rows and columns is not consistent.");
+			}
+			else {
+				numRows++;
+			}
+		}
+		
+		//If this point is reached, the rows all have the same number of columns
+		numColumns = expectedCommas + 1; //There is one less comma then total columns
+
+		board = new BoardCell[numRows][numColumns];
+		
+		//TODO At this point the grid is consistent, now create the boardcells
 	}
-	
+
 	public void calcAdjacencies() {
 		// Generates map of all cells and their adjacent cells and stores it in adjMtx
 		BoardCell currCell;
@@ -106,7 +152,7 @@ public class Board {
 			}
 		}
 	}
-	
+
 	public void calcTargets(BoardCell cell, int pathLength) {
 		//Stores possible target cells in targets
 		Set<BoardCell> adj = getAdjList(cell);
@@ -128,38 +174,38 @@ public class Board {
 			}
 		}
 	}
-	
+
 	// -- Getters and Setters --
-	
+
 	// This method returns the only Board.
 	public static Board getInstance() {
 		return theInstance;
 	}
-	
+
 	public Map<Character, String> getLegend() {
 		return legend;
 	}
-	
+
 	public int getNumRows() {
 		return numRows;
 	}
-	
+
 	public int getNumColumns() {
 		return numColumns;
 	}
-	
+
 	public Set<BoardCell> getTargets() {
 		return targets; //Getter for targets so the set doesn't have to be created for every get
 	}
-	
+
 	public Set<BoardCell> getAdjList(BoardCell cell) {
 		return adjMatrix.get(cell); //Stores the adjacency list for a cell in a new list
 	}
-	
+
 	public BoardCell getCellAt(int i, int j) {
 		return board[i][j]; //returns the BoardCell stored at the specified value
 	}
-	
+
 	public void setConfigFiles(String boardCfg, String roomCfg) {
 		boardConfigFile = boardCfg;
 		roomConfigFile = roomCfg;
