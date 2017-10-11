1af8737e8e8da016e7c80af18685c9248ea35ce3
Nathaniel Fuller <nfuller@mines.edu>
2017-10-11 09:07:28 -0600

    Fixed errors in our test file. ClueBoardTests all pass

--- src/tests/ClueBoardTests.java
+++ src/tests/ClueBoardTests.java
@@ -13,8 +13,8 @@ import clueGame.BoardCell;
 public class ClueBoardTests {
 
 	public static final int LEGEND_SIZE = 11;
-	public static final int NUM_ROWS = 20;
-	public static final int NUM_COLUMNS = 25;
+	public static final int NUM_ROWS = 25;
+	public static final int NUM_COLUMNS = 20;
 	
 	private static Board board;
 	
@@ -31,17 +31,17 @@ public class ClueBoardTests {
 	@Test
 	public void testDoorwayDirections() {
 		// Test doorways in each direction, as well as cells with no doorways.
-		BoardCell door = board.getCellAt(4, 2); // Kitchen door (Down)
+		BoardCell door = board.getCellAt(2, 4); // Kitchen door (Down)
 		assertEquals(DoorDirection.DOWN, door.getDoorDirection());
-		door = board.getCellAt(14, 2); // Ballroom door (Left)
+		door = board.getCellAt(2, 14); // Ballroom door (Left)
 		assertEquals(DoorDirection.LEFT, door.getDoorDirection());
-		door = board.getCellAt(3,22); // Library door (Right)
+		door = board.getCellAt(22, 3); // Library door (Right)
 		assertEquals(DoorDirection.RIGHT, door.getDoorDirection());
-		door = board.getCellAt(17, 20); // Master Bedroom door (Up)
+		door = board.getCellAt(20, 17); // Master Bedroom door (Up)
 		assertEquals(DoorDirection.UP, door.getDoorDirection());
-		door = board.getCellAt(0, 10); // Cell in Servant's Quarters (None)
+		door = board.getCellAt(10, 0); // Cell in Servant's Quarters (None)
 		assertEquals(DoorDirection.NONE, door.getDoorDirection());
-		door = board.getCellAt(19, 11); // Cell in Art Room (None)
+		door = board.getCellAt(11, 19); // Cell in Art Room (None)
 		assertEquals(DoorDirection.NONE, door.getDoorDirection());
 	}
 	
@@ -81,21 +81,21 @@ public class ClueBoardTests {
 		// Test a cell from each room to make sure they have the proper initials.
 		BoardCell cell = board.getCellAt(0, 0); // Kitchen (K)
 		assertEquals('K', cell.getInitial());
-		cell = board.getCellAt(10, 0); // Conservatory (C)
+		cell = board.getCellAt(0, 10); // Conservatory (C)
 		assertEquals('C', cell.getInitial());
-		cell = board.getCellAt(19, 0); // Ballroom (B)
+		cell = board.getCellAt(0, 19); // Ballroom (B)
 		assertEquals('B', cell.getInitial());
-		cell = board.getCellAt(0, 12); // Servant's Quarters (Q)
+		cell = board.getCellAt(12, 0); // Servant's Quarters (Q)
 		assertEquals('Q', cell.getInitial());
-		cell = board.getCellAt(19, 11); // Art Room (A)
+		cell = board.getCellAt(11, 19); // Art Room (A)
 		assertEquals('A', cell.getInitial());
-		cell = board.getCellAt(19, 16); // Trophy Room (R)
+		cell = board.getCellAt(16, 19); // Trophy Room (R)
 		assertEquals('R', cell.getInitial());
-		cell = board.getCellAt(0, 19); // Library (L)
+		cell = board.getCellAt(19, 0); // Library (L)
 		assertEquals('L', cell.getInitial());
-		cell = board.getCellAt(11, 19); // Theatre (T)
+		cell = board.getCellAt(20, 11); // Theatre (T)
 		assertEquals('T', cell.getInitial());
-		cell = board.getCellAt(19, 19); // Master Bedroom (M)
+		cell = board.getCellAt(20, 19); // Master Bedroom (M)
 		assertEquals('M', cell.getInitial());
 	}
 
