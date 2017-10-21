496d1aedac7f36ddf56fe1e5696a4508218d6b20
Adam Nelson <[adamnelson@mines.edu]>
2017-10-21 10:26:53 -0600

    Added board constructor block

--- src/tests/BoardAdjTests.java
+++ src/tests/BoardAdjTests.java
@@ -2,11 +2,23 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import clueGame.Board;
 
 public class BoardAdjTests {
 
+	//Constructor
+	private static Board board;
+	@BeforeClass
+	public static void setUp() {
+		// Board is singleton, get the only instance and initialize it		
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueGameLayout.csv", "ClueGameLegend.txt");		
+		board.initialize();
+	}
+	
 	// TODO: Implement tests for only walkways adjacent
 	@Test
 	public void testWalkways() {
