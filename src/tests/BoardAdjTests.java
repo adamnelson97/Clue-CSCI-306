4a02c43deb8c2248937d19441f2e268b9073816f
Nathaniel Fuller <nfuller@mines.edu>
2017-10-21 10:26:56 -0600

    Added test stubs for adjacency tests.

--- src/tests/BoardAdjTests.java
+++ src/tests/BoardAdjTests.java
@@ -13,6 +13,34 @@ public class BoardAdjTests {
 		
 	}
 	
-	//TODO: Implement tests for 
+	// TODO: Implement tests for locations within rooms
+	@Test
+	public void testInsideRoom() {
+		
+	}
+	
+	// TODO: Implement tests for edges
+	@Test
+	public void testBoardEdges() {
+		
+	}
+	
+	// TODO: Implement tests for locations next to rooms
+	@Test
+	public void testRoomEdges() {
+		
+	}
+	
+	// TODO: Implement tests for locations next to doorways
+	@Test
+	public void testDoorwayEntry() {
+		
+	}
+	
+	// TODO: Implement tests for doorways
+	@Test
+	public void testDoorwayExit() {
+		
+	}
 
 }
