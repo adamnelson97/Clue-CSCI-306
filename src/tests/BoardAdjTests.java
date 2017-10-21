d222178b323b6c8b6c1bf1e961e74f31c990afbe
Nathaniel Fuller <nfuller@mines.edu>
2017-10-21 10:12:40 -0600

    Created JUnit test file for adjacencies and targets.

---
+++ src/tests/BoardAdjTests.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import clueGame.Board;
+
+public class BoardAdjTests {
+
+	// TODO: Implement tests for only walkways adjacent
+	@Test
+	public void testWalkways() {
+		
+	}
+	
+	//TODO: Implement tests for 
+
+}
