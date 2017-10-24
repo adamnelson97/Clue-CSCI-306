f27618cb37682f0e9a139023fae3095d83ac01c0
Adam Nelson <adamnelson@mines.edu>
2017-10-23 22:09:15 -0600

    Added javadoc comments

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,7 +1,7 @@
 /*
  * Class: BadConfigFormatException
  * 
- * Authors: Nathaniel Fuller, Adam Nelson
+ * Authors: Nathaniel Fuller, Adam Nelson, Youjun Lee
  * 
  * Purpose: An exception to be thrown when reading data from an improperly formatted config file.
  * 
@@ -12,14 +12,32 @@ package clueGame;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
+/**
+ * <h1>BadConfigFormatException</h1>
+ * This is a custom exception that tells the user if a legend file or board
+ * layout file has a problem.
+ * 
+ * @author Adam Nelson, Nathaniel Fuller, Youjun Lee
+ * @version 1.0
+ * @since 2017-10-09
+ *
+ */
 public class BadConfigFormatException extends Exception {
 	// -- Variables --
 
 	// -- Constructors --
+	
+	/**
+	 * Constructor with generic message.
+	 */
 	public BadConfigFormatException() {
 		super("Error: Bad Configuration Files");
 	}
 
+	/**
+	 * Constructor that prints a custom error message.
+	 * @param s This string is determined in the method the exception is called from.
+	 */
 	public BadConfigFormatException(String s) {
 		super(s);
 		PrintWriter writer;
