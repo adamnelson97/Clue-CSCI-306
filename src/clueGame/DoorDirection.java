b54deeeaeca089302c561fc74eaa0119a40e0176
Adam Nelson <adamnelson@mines.edu>
2017-10-23 21:53:49 -0600

    Added javadoc comments

--- src/clueGame/DoorDirection.java
+++ src/clueGame/DoorDirection.java
@@ -9,7 +9,21 @@
 
 package clueGame;
 
+/**
+ * <h1>Door Direction</h1>
+ * Enumerated type for identifying which way a door
+ * opens up on the board.
+ * 
+ * @author Adam Nelson, Nathaniel Fuller, Youjun Lee
+ * @version 1.0
+ * @since 2017-10-09
+ *
+ */
 public enum DoorDirection {
+	/**
+	 * There are four possible directions specifying how the player
+	 * is adjacent to a door.
+	 */
 	UP, DOWN, LEFT, RIGHT, NONE;
 	
 	
