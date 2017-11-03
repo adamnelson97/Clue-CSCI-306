f7cefff1f9dbcf4100efe11150771c293fc4b3ea
Adam Nelson <adamnelson@mines.edu>
2017-11-02 22:51:43 -0600

    Added Player class shell.

---
+++ src/clueGame/Player.java
@@ -0,0 +1,35 @@
+package clueGame;
+
+import java.awt.Color;
+
+/**
+ * <h1>Player</h1>
+ * This class stores all the information relevant to the players in the game.
+ * @author Adam Nelson, Youjun Lee
+ * @version 1.0
+ * @since 2017-11-2
+ *
+ */
+public class Player {
+
+	//Variables
+	private String playerName;
+	private int row;
+	private int column;
+	private Color color;
+	
+	//Constructors
+	
+	
+	//Methods
+	
+	/**
+	 * Reveals a suggested card to the player making the suggestion.
+	 * @param suggestion The proposed suggestion, e.g. PERSON did it with the WEAPON in the ROOM.
+	 * @return Card A suggested card that the player contains in their hand.
+	 */
+	public Card disproveSuggestion(Solution suggestion) {
+		//TODO Complete disproveSuggestion method
+		return null;
+	}
+}
