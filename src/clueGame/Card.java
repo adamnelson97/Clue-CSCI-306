af7ca428368ffd816a5f06560e35fb1ad9202842
Adam Nelson <adamnelson@mines.edu>
2017-11-02 23:17:31 -0600

    Updated JavaDoc author comments.

--- src/clueGame/Card.java
+++ src/clueGame/Card.java
@@ -4,7 +4,7 @@ package clueGame;
  * <h1>Card</h1>
  * This class stores the information for a single card,
  * which can be a person, weapon, or room.
- * @author Adam Nelson
+ * @author Adam Nelson, Youjun Lee
  * @version 1.0
  * @since 2017-11-02
  *
