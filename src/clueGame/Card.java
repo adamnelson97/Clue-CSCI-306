5e3f1433e8cdace7200d9f5faf599b52f47ee6bd
Adam Nelson <adamnelson@mines.edu>
2017-11-02 23:07:51 -0600

    Added Card class shell.

---
+++ src/clueGame/Card.java
@@ -0,0 +1,34 @@
+package clueGame;
+
+/**
+ * <h1>Card</h1>
+ * This class stores the information for a single card,
+ * which can be a person, weapon, or room.
+ * @author Adam Nelson
+ * @version 1.0
+ * @since 2017-11-02
+ *
+ */
+public class Card {
+	
+	//Variables
+	private String cardName;
+	public enum CardType {
+		PERSON, WEAPON, ROOM;
+	}
+	private CardType cardType;
+	
+	//Constructors
+	
+	//Methods
+	
+	/**
+	 * Comparator that determines if two cards are equal.
+	 * @return boolean Whether the two cards are equal.
+	 */
+	public boolean equals() {
+		//TODO complete equals method
+		return null;
+	}
+
+}
