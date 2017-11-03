9f177a41b9edd1bab848cc2c7170788683562266
Adam Nelson <adamnelson@mines.edu>
2017-11-02 23:14:37 -0600

    Added Solution class shell.

---
+++ src/clueGame/Solution.java
@@ -0,0 +1,21 @@
+package clueGame;
+
+/**
+ * <h1>Solution</h1>
+ * This class stores the solution to the game.
+ * @author Adam Nelson, Youjun Lee
+ * @version 1.0
+ * @since 2017-11-02
+ *
+ */
+public class Solution {
+
+	//Variables
+	public String person;
+	public String room;
+	public String weapon;
+	
+	//Constructors
+	
+	//Methods
+}
