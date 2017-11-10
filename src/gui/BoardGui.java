2aa2a541d7b396081df9e257175c186d335309ff
Adam Nelson <[adamnelson@mines.edu]>
2017-11-10 15:08:33 -0700

    Created BoardGui class, no constructor or methods yet.

---
+++ src/gui/BoardGui.java
@@ -0,0 +1,20 @@
+package gui;
+
+import javax.swing.JPanel;
+
+/**
+ * <h1>BoardGui</h1>
+ * BoardGui is the upper section of the main GUI
+ * used to play the game. In includes panels displaying what cards
+ * the player currently possesses, as well as the game board.
+ * @author Adam Nelson, Youjun Lee
+ * @since 2017-11-10
+ *
+ */
+public class BoardGui extends JPanel {
+
+	public BoardGui() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
