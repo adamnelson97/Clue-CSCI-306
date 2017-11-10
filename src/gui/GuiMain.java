7e7bfcc4a7b89a97c43af6b3ed60606774cc591b
Adam Nelson <adamnelson@mines.edu>
2017-11-10 14:12:36 -0700

    Added a main method that calls a JFrame and sets the size, close operation, title, and visibility.

--- src/gui/GuiMain.java
+++ src/gui/GuiMain.java
@@ -1,5 +1,28 @@
 package gui;
 
-public class GuiMain {
+import javax.swing.JFrame;
 
-}
+/**
+ * <h1>GuiMain</h1>
+ * This class creates a JFrame, calls constructors
+ * for the Control GUI and Board GUI and then begins the game.
+ * @author Adam Nelson, Youjun Lee
+ * @since 2017-11-10
+ * @see ControlGui
+ *
+ */
+public class GuiMain {
+	
+	
+	public static void main(String[] args) {
+		//Create JFrame window
+		JFrame frame = new JFrame();
+		//Set size and default close operation
+		frame.setSize(800, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Set name of window
+		frame.setTitle("Clue Game");
+		//Make window visible
+		frame.setVisible(true);
+	}
+} //End of Class
