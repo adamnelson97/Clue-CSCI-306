45fce543fad189d6389c7ab80981e70de5648c8d
Adam Nelson <adamnelson@mines.edu>
2017-11-15 20:56:56 -0700

    Added new custom dialog where the user will select what player they want to be.

---
+++ src/gui/PlayerChoiceDialog.java
@@ -0,0 +1,13 @@
+package gui;
+
+/**
+ * <h1>PlayerChoiceDialog</h1>
+ * Custom dialog where the user chooses what
+ * character they will be in the game.
+ * @author Adam Nelson, Youjun Lee
+ * @since 2017-11-15
+ *
+ */
+public class PlayerChoiceDialog extends JDialog {
+
+}
