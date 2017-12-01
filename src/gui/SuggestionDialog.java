75a9e8acdf924fb08e4308a9cf031fea8283139f
Adam Nelson <adamnelson@mines.edu>
2017-12-01 13:48:36 -0700

    Working on constructor for suggestion dialog. Added some labels and combo box.

--- src/gui/SuggestionDialog.java
+++ src/gui/SuggestionDialog.java
@@ -2,8 +2,14 @@ package gui;
 
 import java.awt.GridLayout;
 
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import clueGame.Board;
+import clueGame.BoardCell;
 
 /**
  * <h1>SuggestionDialog</h1>
@@ -17,8 +23,10 @@ public class SuggestionDialog extends JDialog {
 	
 	/**
 	 * Default constructor.
+	 * @param currentLoc The current cell the player is on.
+	 * @param theBoard The game board.
 	 */
-	public SuggestionDialog() {
+	public SuggestionDialog(BoardCell currentLoc, Board theBoard) {
 		super();
 		JPanel panel = new JPanel();
 		setContentPane(panel);
@@ -27,21 +35,57 @@ public class SuggestionDialog extends JDialog {
 		setTitle("Make a guess");
 		
 		panel.setLayout(new GridLayout(4,2));
+		
 		//Add label for room.
+		panel.add(new JLabel("Your room"));
 		
 		//Add non-editable JTextField for the current room.
+		JTextField room = new JTextField(10);
+		room.setEditable(false); //The player should not be able to type in this area.
+		//Retrieve the name of the room from the initial of the current cell.
+		room.setText(theBoard.getLegend().get(currentLoc.getInitial()));
+		panel.add(room);
 		
 		//Add label for the person guess.
+		panel.add(new JLabel("Person"));
 		
 		//Add combo box to select the person.
+		JComboBox<String> person = personBox();
+		panel.add(person);
 		
 		//Add label for the weapon guess.
+		panel.add(new JLabel("Weapon"));
 		
 		//Add combo box to select the weapon.
+		JComboBox weapon = new JComboBox();
+		panel.add(weapon);
 		
 		//Add button to submit suggestion.
 		
 		//Add button to cancel.
+		
+	}
+	
+	/**
+	 * Creates a JComboBox with options for each player.
+	 * @return JComboBox The combo box.
+	 */
+	private JComboBox<String> personBox() {
+		JComboBox<String> person = new JComboBox<String>();
+		person.addItem("Miss Scarlett");
+		person.addItem("Professor Plum");
+		person.addItem("Mrs. Peacock");
+		person.addItem("Mr. Green");
+		person.addItem("Colonel Mustard");
+		person.addItem("Mrs. White");
+		return person;
+	}
+	
+	/**
+	 * Creates a JComboBox with options for each weapon.
+	 * @return JComboBox The combo box.
+	 */
+	private JComboBox<String> weaponBox() {
 		
 	}
 
